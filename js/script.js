e5df907ab820749656f75b17173e59a48b14a5fa
Decagon <decagon@Decagons-MacBook-Air.local>
2022-02-06 03:12:46 +0100

    my portfolio

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Web Designer",
+    "Web Developer",
+    "Software Engineer",
+    "YouTuber",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
